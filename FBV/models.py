068cbdc09e3046c441fda8aaddb9881881ff7574
shubhamjain31 <sj27754@gmail.com>
2022-07-10 20:24:10 +0530

    function-based views

--- FBV/models.py
+++ FBV/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    name            = models.CharField(max_length=30)
+    email           = models.EmailField()
+    password        = models.CharField(max_length=40)
+    phone           = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name        = models.CharField(max_length=30)
+    author      = models.CharField(max_length=40)
+    price       = models.IntegerField()
+    discount    = models.IntegerField(default=0)
+    duration    = models.FloatField()
+
+    def __str__(self):
+        return self.name
--- FBV/views.py
+++ FBV/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from .models import Employee
+from .serializers import EmployeeSerializer
 
 # Create your views here.
+
+@csrf_exempt
+def employeeListView(request, pk=None):
+    if request.method == 'GET':
+        employees   = Employee.objects.all()
+        serializer  = EmployeeSerializer(employees, many=True)
+
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data_           = JSONParser().parse(request)
+        serializer      = EmployeeSerializer(data=data_)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, safe=False)
+        else:
+            return JsonResponse(serializer.errors, safe=False)
+
+    elif request.method == 'PUT':
+        employee_obj    = Employee.objects.get(pk=pk)
+
+        data_           = JSONParser().parse(request)
+        serializer      = EmployeeSerializer(employee_obj, data=data_)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, safe=False)
+        else:
+            return JsonResponse(serializer.errors, safe=False)
+
+    elif request.method == 'DELETE':
+        employee_obj    = Employee.objects.get(pk=pk)
+        employee_obj.delete()
+
+        return JsonResponse({}, status=204)
