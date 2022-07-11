0c0fc36fd6c6c0da6d9fa382598e83d1c0019a83
shubhamjain31 <sj27754@gmail.com>
2022-07-11 10:32:33 +0530

    function-based views using apiview

--- FBV/urls.py
+++ FBV/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/employees/<str:pk>', views.employeeListView, name='employeeListView')
+    path('api/simple/employees/<str:pk>', views.employeeListView, name='employeeListView'),
+    path('api/apiview/employees/<str:pk>', views.employeeListAPIView, name='employeeListAPIView')
 ]
--- FBV/views.py
+++ FBV/views.py
@@ -1,12 +1,15 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Employee
 from .serializers import EmployeeSerializer
 
 # Create your views here.
 
+# ------------------------------------------------- @ Simple API @ -----------------------------------------------
 @csrf_exempt
 def employeeListView(request, pk=None):
     if request.method == 'GET':
@@ -42,3 +45,53 @@ def employeeListView(request, pk=None):
         employee_obj.delete()
 
         return JsonResponse({}, status=204)
+
+# ------------------------------------------------- @ APIView @ -----------------------------------------------
+@api_view(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+def employeeListAPIView(request, pk=None):
+    if request.method == 'GET':
+        if pk is None or pk == 'None':
+            employees   = Employee.objects.all()
+        else:
+            employees   = Employee.objects.filter(pk=pk)
+        serializer  = EmployeeSerializer(employees, many=True)
+
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        # data_           = JSONParser().parse(request)
+        serializer      = EmployeeSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    elif request.method == 'PUT':
+        employee_obj    = Employee.objects.get(pk=pk)
+
+        serializer      = EmployeeSerializer(employee_obj, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    elif request.method == 'PATCH':
+        employee_obj    = Employee.objects.get(pk=pk)
+
+        serializer      = EmployeeSerializer(employee_obj, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    elif request.method == 'DELETE':
+        employee_obj    = Employee.objects.get(pk=pk)
+        employee_obj.delete()
+
+        return Response({}, status=204)
